aa0b17e066b708d2925dd995e46f975e0b744e93
CodeWithJoe2020 <75099121+CodeWithJoe2020@users.noreply.github.com>
2020-12-16 19:39:52 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,45 @@
+import tweepy
+import sys
+import requests
+import json
+import time
+
+
+API_KEY = "t1UswbPBdyBDM8ECMD1B49SGZ"
+SECRET_KEY =  "7LTT5vGL8WmUMV3g3lKoYXCX6dzxDS3qLHnzlRysCMYIYrEOMD"
+ACCESS_TOKEN = "1333847252893388801-CJ0bpmErsFvBwJXr07YDvdlUivr0QR"
+ACCESS_TOKEN_SECRET = "kavew7S0Wf4U4Le31OHgnoMrcxlBqU1cgJJultfBkaaB8"
+
+auth_handler = tweepy.OAuthHandler(consumer_key=API_KEY, consumer_secret=SECRET_KEY)
+auth_handler.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth_handler, wait_on_rate_limit=True)
+
+#me = api.me()   Personal Info
+print('logged in succesfully')
+
+while True:
+    etherprice = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd')
+    bitcoinprice = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
+    xrpprice = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=ripple&vs_currencies=usd')
+    price = etherprice.json()
+    bitcoin = bitcoinprice.json()
+    xrp = xrpprice.json()
+    priceusd = price['ethereum']['usd']
+    btcusd = bitcoin['bitcoin']['usd']
+    xrpusd = xrp['ripple']['usd']
+
+    message = 'current #Ethereum Price: ' + str(priceusd) + ' USD\nTrade #Ethereum  \nnow at 🚀https://bit.ly/3mpFwii - Bybit 🚀 \n #crypto #eth #earncrypto #tothemoon'
+    message2 = 'current #BTC Price ' + str(btcusd) + ' USD \n Go long on #BTC now on BYBIT 🚀https://bit.ly/3mpFwii🚀🚀'
+
+    api.update_status(message)
+    print(priceusd)
+    print('tweet was posted')
+    time.sleep(2)
+    api.update_status(message2)
+    print('tweet was sent')
+    time.sleep(2)
+    if btcusd > int(20800):
+        alltime = f'Bitcoin just reached ALL TIME HIGH 🚀🚀🚀 Price: {btcusd} \nBuy #bitcoin on #Bitpanda https://bit.ly/3agRA2V '
+        api.update_status(alltime)
+    time.sleep(300)
